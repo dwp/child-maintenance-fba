333f0e50b5f4248b4f854cb26cb82f0316d8a15c
James Seabright <james.seabright@engineering.digital.dwp.gov.uk>
2026-06-29 23:32:23 +0100

    Finished building v3 prototype
    GitOrigin-RevId: f1d6ed884c491cbbaeec1e83d1bfa0fe751390d5

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,8 @@ const router = govukPrototypeKit.requests.setupRouter();
 // Mount versions
 router.use('/v1', require('./views/v1/routing')());
 router.use('/v2', require('./views/v2/routing')());
+require('./views/v3/routing')(router);
+
+
 
 module.exports = router;
--- app/views/v3/routing.js
+++ app/views/v3/routing.js
@@ -1,17 +1,58 @@
-//
-// For guidance on how to create routes see:
-// https://prototype-kit.service.gov.uk/docs/create-routes
-//
+module.exports = function (router) {
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+  router.post('/v3/arrangement/arrangement-type-answer', function (req, res) {
+    var arrangementType = req.session.data['arrangement-type'];
 
-router.post('/live-in-uk-answer', function(request, response) {
-
-    var liveInUK = request.session.data['live-in-uk']
-    if (liveInUK == "Yes"){
-        response.redirect("/next-question")
-    } else {
-        response.redirect("/ineligible")
+    if (arrangementType === "regular" || arrangementType === "both") {
+      res.redirect("regular-payment");
+    } else if (arrangementType === "shared") {
+      res.redirect("shared-costs");
+    } else if (arrangementType === "other") {
+      res.redirect("what-is-your-arrangement");
     }
-})
+  });
+
+  router.post('/v3/arrangement/regular-and-shared-check', function (req, res) {
+    var regularAndShared = req.session.data['arrangement-type'];
+
+    if (regularAndShared == "both"){
+      res.redirect("shared-costs");
+    } else {
+      res.redirect("anything-else");
+    }
+  });
+
+  router.post('/v3/arrangement/varied-check', function (req, res) {
+    var variedCheck = req.session.data['shared-cost-payment'];
+
+    if (variedCheck == "varied"){
+      res.redirect("varied-costs");
+    } else {
+      res.redirect("anything-else");
+    }
+  });  
+
+  router.post('/v3/arrangement/save-online-answer', function (req, res) {
+    var saveOnline = req.session.data['save-online-decision'];
+
+    if (saveOnline == "yes") {
+
+      req.session.data['arrangementSaved'] = 'yes';
+
+      res.redirect("one-login");
+    } else {
+      res.redirect("set-reminder");
+    }
+  });
+
+  router.post('/v3/arrangement/set-reminder-answer', function (req, res) {
+    var setReminder = req.session.data['setReminder'];
+
+    if (setReminder == "yes"){
+      res.redirect("output-reminder");
+    } else {
+      res.redirect("output-download");
+    }
+  });      
+  
+};
